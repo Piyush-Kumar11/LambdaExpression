4ad9d520f372fb6d456d8579383049d5df39713f
Piyush <piushverma111@gmail.com>
2024-12-26 11:45:44 +0530

    Added more LINQ functions

---
+++ Example2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpression
+{
+    internal class Example2
+    {
+        static List<int> num = new List<int>();
+        public Example2()
+        {
+            num.Add(22);
+            num.Add(55);
+            num.Add(11);
+            num.Add(22);
+            num.Add(44);
+            num.Add(33);
+            num.Add(66);
+        }
+
+        public void EvenNum()
+        {
+            var evenNum = num.FindAll(x => x % 2 == 0);
+            Console.WriteLine("Even: ");
+            foreach (var x in evenNum)
+            {
+                Console.WriteLine(x);
+            }
+        }
+
+        public void SortList()
+        {
+            var sorted = num.OrderBy(x => x);
+            Console.WriteLine("Sorted List: ");
+            foreach (var x in sorted)
+            {
+                Console.WriteLine(x);
+            }
+        }
+
+        public void IfPresent()
+        {
+            bool check = num.Any(x => x == 22);
+            if (check){
+                Console.WriteLine("22 is Present!"); 
+            }
+            else{
+                Console.WriteLine("22 is Not Present!"); 
+            }
+        }
+        public void RemoveDuplicates()
+        {
+            var distinct = num.Distinct();
+            Console.WriteLine("After Removing Duplicates: ");
+
+            foreach(var x in distinct)
+            {
+                Console.WriteLine(x);
+            }
+        }
+        public void FindFirstElement()
+        {
+            int firstOccurance = num.First(x => x == 22);
+            Console.WriteLine(firstOccurance);
+        }
+
+        public void DefaultExpression()
+        {
+            Console.WriteLine(num.FirstOrDefault(x => x == 22));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,18 +9,27 @@ namespace LambdaExpression
     internal class Program
     {
         static Example e = new Example();
+        static Example2 e2 = new Example2();
+
         static void Main(string[] args)
         {
-            e.ForEachExpression();
-            e.OrderByExpression();
-            e.SelectExpression();
-            e.WhereExpression();
-            e.CountExpression();
-            e.AnyExpression();
-            e.DistinctExpression();
-            e.FindAllExpression();
-            e.OrderByDescendingExpression();
-            e.ToListExpression();
+            //e2.EvenNum();
+            //e2.SortList();
+            //e2.IfPresent();
+            //e2.RemoveDuplicates();
+            //e2.FindFirstElement();
+            e2.DefaultExpression();
+
+            //e.ForEachExpression();
+            //e.OrderByExpression();
+            //e.SelectExpression();
+            //e.WhereExpression();
+            //e.CountExpression();
+            //e.AnyExpression();
+            //e.DistinctExpression();
+            //e.FindAllExpression();
+            //e.OrderByDescendingExpression();
+            //e.ToListExpression();
         }
     }
 }
